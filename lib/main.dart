45ff8ca412116497a52a1f70ab59affb0e12368b
Unique-chiemerie <lilanimate1@gmail.com>
2023-07-15 19:17:24 +0100

    finalized

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,8 @@ final colorprovider = StateProvider<Color>(
   ),
 );
 final marginprovider = StateProvider((ref) => Random().nextDouble() * 64);
+final heightprovider = StateProvider((ref) => Random().nextDouble() * 64);
+final widthprovider = StateProvider((ref) => Random().nextDouble() * 64);
 
 void main() {
   runApp(
@@ -31,37 +33,67 @@ void main() {
 class mainapp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final margin = ref.watch(marginprovider);
-    final borda = ref.watch(borderprovider);
-    final colorz = ref.watch(colorprovider);
-    return Column(
-      children: [
-        const SizedBox(
-          height: 70,
-        ),
-        AnimatedContainer(
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(
-                Radius.circular(borda),
-              ),
+    var margin = ref.watch(marginprovider);
+    var borda = ref.watch(borderprovider);
+    var colorz = ref.watch(colorprovider);
+    var height = ref.watch(heightprovider);
+    var width = ref.watch(widthprovider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Riverpod morph test'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 70,
             ),
-            margin: EdgeInsets.all(margin),
-            color: colorz,
-            duration: duration),
-        const SizedBox(
-          height: 70,
+            AnimatedContainer(
+                height: height,
+                width: width,
+                decoration: BoxDecoration(
+                  color: colorz,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(borda),
+                  ),
+                ),
+                margin: EdgeInsets.all(margin),
+                duration: duration),
+            const SizedBox(
+              height: 70,
+            ),
+            IconButton(
+              onPressed: () {
+                ref.read(colorprovider.notifier).update(
+                      (state) => Color.fromARGB(
+                        Random().nextInt(250),
+                        Random().nextInt(250),
+                        Random().nextInt(250),
+                        Random().nextInt(250),
+                      ),
+                    );
+                ref
+                    .read(borderprovider.notifier)
+                    .update((state) => borda + Random().nextDouble());
+                ref.read(marginprovider.notifier).update(
+                      (state) => margin + Random().nextDouble(),
+                    );
+
+                ref
+                    .read(heightprovider.notifier)
+                    .update((state) => height = Random().nextDouble() * 100);
+                ref
+                    .read(widthprovider.notifier)
+                    .update((state) => width = Random().nextDouble() * 100);
+              },
+              icon: const Icon(Icons.change_circle),
+            ),
+          ],
         ),
-        IconButton(
-          onPressed: () {
-            ref.read(colorprovider.notifier).update((state) => colorz);
-            ref.read(borderprovider.notifier).update((state) => borda);
-            ref.watch(marginprovider.notifier).update((state) => margin);
-          },
-          icon: const Icon(Icons.change_circle),
-        ),
-      ],
+      ),
     );
   }
 }
